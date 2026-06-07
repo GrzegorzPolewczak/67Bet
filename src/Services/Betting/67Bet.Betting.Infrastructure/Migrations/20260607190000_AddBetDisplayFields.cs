1c280cf7531012c309d004d94cbc4757b790f5fa
GrzegorzPolewczak <148885875+GrzegorzPolewczak@users.noreply.github.com>
2026-06-07 20:35:34 +0200

    Dodanie migracji

---
+++ src/Services/Betting/67Bet.Betting.Infrastructure/Migrations/20260607190000_AddBetDisplayFields.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _67Bet.Betting.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBetDisplayFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OutcomeName",
+                table: "Bets",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MarketName",
+                table: "Bets",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EventName",
+                table: "Bets",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartTime",
+                table: "Bets",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "Bets");
+
+            migrationBuilder.DropColumn(
+                name: "EventName",
+                table: "Bets");
+
+            migrationBuilder.DropColumn(
+                name: "MarketName",
+                table: "Bets");
+
+            migrationBuilder.DropColumn(
+                name: "OutcomeName",
+                table: "Bets");
+        }
+    }
+}
